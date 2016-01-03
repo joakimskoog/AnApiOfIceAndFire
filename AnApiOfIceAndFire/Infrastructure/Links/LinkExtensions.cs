599569fb08de6062a9b022357156b4dfdc54d267
Joakim Skoog <glasvegas@hotmail.se>
2016-01-03 17:43:57 +0100

    Removed commented code

--- AnApiOfIceAndFire/Infrastructure/Links/LinkExtensions.cs
+++ AnApiOfIceAndFire/Infrastructure/Links/LinkExtensions.cs
@@ -14,11 +14,6 @@ namespace AnApiOfIceAndFire.Infrastructure.Links
             if (!string.IsNullOrEmpty(link.Relation))
             {
                 AppendParameter("rel", link.Relation, builder);
-                //builder.Append(";");
-                //builder.Append("rel=");
-                //builder.Append('"');
-                //builder.Append(link.Relation);
-                //builder.Append('"');
             }
 
             return builder.ToString();
