562fffec51eeda7d05cd9bbf50e1738a617d45a9
Joakim Skoog <glasvegas@hotmail.se>
2016-01-06 13:15:29 +0100

    Added fields to ICharacter

--- AnApiOfIceAndFire.Domain/Models/ICharacter.cs
+++ AnApiOfIceAndFire.Domain/Models/ICharacter.cs
@@ -1,7 +1,28 @@
-﻿namespace AnApiOfIceAndFire.Domain.Models
+﻿using System.Collections.Generic;
+
+namespace AnApiOfIceAndFire.Domain.Models
 {
     public interface ICharacter
     {
-         int Identifier { get; }
+        int Identifier { get; }
+        string Name { get; }
+        string Culture { get; }
+        string Born { get; }
+        string Died { get; }
+        IReadOnlyCollection<string> Titles { get; }
+        IReadOnlyCollection<string> Aliases { get; }
+
+        ICharacter Father { get; }
+        ICharacter Mother { get; }
+        IReadOnlyCollection<ICharacter> Children { get; }
+        ICharacter Spouse { get; }
+
+        IReadOnlyCollection<IHouse> Allegiances { get; }
+
+        IReadOnlyCollection<IBook> Books { get; }
+        IReadOnlyCollection<IBook> PovBooks { get; }
+
+        IReadOnlyCollection<string> TvSeries { get; }
+        IReadOnlyCollection<string> PlayedBy { get; }
     }
 }
