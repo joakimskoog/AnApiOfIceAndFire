b6a52ae49642dcf808b172e2f73e6e62638c52da
Joakim Skoog <glasvegas@hotmail.se>
2016-01-06 12:46:55 +0100

    Some files that were missing from the previous commit

--- AnApiOfIceAndFire.Domain/HouseService.cs
+++ AnApiOfIceAndFire.Domain/HouseService.cs
@@ -1,7 +1,24 @@
-﻿namespace AnApiOfIceAndFire.Domain
+﻿using System.Linq;
+using AnApiOfIceAndFire.Domain.Models;
+using SimplePagination;
+
+namespace AnApiOfIceAndFire.Domain
 {
-    public class HouseService
+    public class HouseService : IModelService<IHouse>
     {
-         
+        public IHouse Get(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IQueryable<IHouse> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IPagedList<IHouse> GetPaginated(int page, int pageSize)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
