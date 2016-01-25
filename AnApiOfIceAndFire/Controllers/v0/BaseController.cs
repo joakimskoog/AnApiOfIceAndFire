c9cda7bcf60ea8d93285e17cf7ac41bb127ca4f7
Joakim Skoog <glasvegas@hotmail.se>
2016-01-25 17:38:30 +0100

    Testing ai logging

--- AnApiOfIceAndFire/Controllers/v0/BaseController.cs
+++ AnApiOfIceAndFire/Controllers/v0/BaseController.cs
@@ -35,15 +35,15 @@ namespace AnApiOfIceAndFire.Controllers.v0
         public virtual async Task<IHttpActionResult> Get(int id)
         {
             throw new Exception("Testing unhandled exception for Application Insights");
-            var model = await _modelService.GetAsync(id);
-            if (model == null)
-            {
-                return NotFound();
-            }
+            //var model = await _modelService.GetAsync(id);
+            //if (model == null)
+            //{
+            //    return NotFound();
+            //}
 
-            var mappedModel = _modelMapper.Map(model, Url);
+            //var mappedModel = _modelMapper.Map(model, Url);
 
-            return Ok(mappedModel);
+            //return Ok(mappedModel);
         }
 
         [HttpHead]
