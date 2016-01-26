019629144493140076cc29a3956d0fc410ad1038
Joakim Skoog <glasvegas@hotmail.se>
2016-01-26 16:44:49 +0100

    Started working on the feeder that will feed the database with data

---
+++ AnApiOfIceAndFire.Data.Feeder/HouseDto.cs
@@ -0,0 +1,21 @@
+﻿namespace AnApiOfIceAndFire.Data.Feeder
+{
+    public class HouseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string[] Seats { get; set; }
+        public string Region { get; set; }
+        public string CoatOfArms { get; set; }
+        public string Words { get; set; }
+        public string[] Titles { get; set; }
+        public int? CurrentLord { get; set; }
+        public int? Founder { get; set; }
+        public string Founded { get; set; }
+        public int? Heir { get; set; }
+        public int? Overlord { get; set; }
+        public string DiedOut { get; set; }
+        public string[] AncestralWeapons { get; set; }
+        public int[] CadetBranches { get; set; }
+    }
+}
