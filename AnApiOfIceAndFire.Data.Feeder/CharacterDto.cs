3a513b3b5a8923c593871b6dd629818c6171e8de
Joakim Skoog <glasvegas@hotmail.se>
2016-01-29 17:46:44 +0100

    Overrided Equals and GetHashCode in dto-classes so that we can calculate the deltas

--- AnApiOfIceAndFire.Data.Feeder/CharacterDto.cs
+++ AnApiOfIceAndFire.Data.Feeder/CharacterDto.cs
@@ -1,6 +1,9 @@
-﻿namespace AnApiOfIceAndFire.Data.Feeder
+﻿using System;
+using System.Linq;
+
+namespace AnApiOfIceAndFire.Data.Feeder
 {
-    public class CharacterDto
+    public class CharacterDto : IEquatable<CharacterDto>
     {
         public int Id { get; set; }
         public string Name { get; set; }
@@ -19,5 +22,60 @@
         public int[] PovBooks { get; set; }
         public string[] PlayedBy { get; set; }
         public string[] TvSeries { get; set; }
+
+
+        public bool Equals(CharacterDto other)
+        {
+            if (other == null) return false;
+
+            ////Note that SequenceEqual will return false if the items are the same but in different orders.
+            return Id == other.Id
+                   && string.Equals(Name, other.Name)
+                   && string.Equals(Culture, other.Culture)
+                   && Titles.SequenceEqual(other.Titles) 
+                   && OtherTitles.SequenceEqual(other.OtherTitles)
+                   && Aliases.SequenceEqual(other.Aliases)
+                   && string.Equals(Born, other.Born)
+                   && string.Equals(Died, other.Died)
+                   && Father == other.Father
+                   && Mother == other.Mother
+                   && Spouse == other.Spouse
+                   && Children.SequenceEqual(other.Children)
+                   && Allegiances.SequenceEqual(other.Allegiances)
+                   && Books.SequenceEqual(other.Books)
+                   && PovBooks.SequenceEqual(other.PovBooks)
+                   && PlayedBy.SequenceEqual(other.PlayedBy)
+                   && TvSeries.SequenceEqual(other.TvSeries);
+        }
+
+        public override bool Equals(object obj)
+        {
+           return Equals(obj as CharacterDto);
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+
+            hash = hash * 23 + Id.GetHashCode();
+            hash = hash * 23 + Name.GetHashCode();
+            hash = hash * 23 + Culture.GetHashCode();
+            hash = hash * 23 + Titles.GetHashCode();
+            hash = hash * 23 + OtherTitles.GetHashCode();
+            hash = hash * 23 + Aliases.GetHashCode();
+            hash = hash * 23 + Born.GetHashCode();
+            hash = hash * 23 + Died.GetHashCode();
+            hash = hash * 23 + Father.GetHashCode();
+            hash = hash * 23 + Mother.GetHashCode();
+            hash = hash * 23 + Spouse.GetHashCode();
+            hash = hash * 23 + Children.GetHashCode();
+            hash = hash * 23 + Allegiances.GetHashCode();
+            hash = hash * 23 + Books.GetHashCode();
+            hash = hash * 23 + PovBooks.GetHashCode();
+            hash = hash * 23 + PlayedBy.GetHashCode();
+            hash = hash * 23 + TvSeries.GetHashCode();
+
+            return hash;
+        }
     }
 }
--- AnApiOfIceAndFire.Data.Feeder/HouseDto.cs
+++ AnApiOfIceAndFire.Data.Feeder/HouseDto.cs
@@ -1,6 +1,9 @@
-﻿namespace AnApiOfIceAndFire.Data.Feeder
+﻿using System;
+using System.Linq;
+
+namespace AnApiOfIceAndFire.Data.Feeder
 {
-    public class HouseDto
+    public class HouseDto : IEquatable<HouseDto>
     {
         public int Id { get; set; }
         public string Name { get; set; }
@@ -17,5 +20,55 @@
         public string DiedOut { get; set; }
         public string[] AncestralWeapons { get; set; }
         public int[] CadetBranches { get; set; }
+
+        public bool Equals(HouseDto other)
+        {
+            if (other == null) return false;
+
+            ////Note that SequenceEqual will return false if the items are the same but in different orders.
+            return Id == other.Id
+                   && string.Equals(Name, other.Name)
+                   && Seats.SequenceEqual(other.Seats)
+                   && string.Equals(Region, other.Region)
+                   && string.Equals(CoatOfArms, other.CoatOfArms)
+                   && string.Equals(Words, other.Words)
+                   && Titles.SequenceEqual(other.Titles)
+                   && CurrentLord == other.CurrentLord
+                   && Founder == other.Founder
+                   && string.Equals(Founded, other.Founded)
+                   && Heir == other.Heir
+                   && Overlord == other.Overlord
+                   && string.Equals(DiedOut, other.DiedOut)
+                   && AncestralWeapons.SequenceEqual(other.AncestralWeapons)
+                   && CadetBranches.SequenceEqual(other.CadetBranches);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as HouseDto);
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+
+            hash = hash * 23 + Id.GetHashCode();
+            hash = hash * 23 + Name.GetHashCode();
+            hash = hash * 23 + Seats.GetHashCode();
+            hash = hash*23 + Region.GetHashCode();
+            hash = hash * 23 + CoatOfArms.GetHashCode();
+            hash = hash * 23 + Words.GetHashCode();
+            hash = hash * 23 + Titles.GetHashCode();
+            hash = hash * 23 + CurrentLord.GetHashCode();
+            hash = hash * 23 + Founder.GetHashCode();
+            hash = hash * 23 + Founded.GetHashCode();
+            hash = hash * 23 + Heir.GetHashCode();
+            hash = hash * 23 + Overlord.GetHashCode();
+            hash = hash * 23 + DiedOut.GetHashCode();
+            hash = hash * 23 + AncestralWeapons.GetHashCode();
+            hash = hash * 23 + CadetBranches.GetHashCode();
+
+            return hash;
+        }
     }
 }
