e715fcff3c84ac3f5bbfe291f22c63d69a3496ea
Joakim Skoog <glasvegas@hotmail.se>
2016-02-06 11:09:16 +0100

    Removed modernizr, started to use cdn for jquery

--- AnApiOfIceAndFire/App_Start/BundleConfig.cs
+++ AnApiOfIceAndFire/App_Start/BundleConfig.cs
@@ -7,13 +7,19 @@ namespace AnApiOfIceAndFire
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            BundleTable.EnableOptimizations = true;
+            bundles.UseCdn = true;
+
+            var jqueryBundle = new ScriptBundle("~/bundles/jquery", "https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js")
+                .Include("~/Scripts/jquery-{version}.min.js");
+            jqueryBundle.CdnFallbackExpression = "window.jQuery";
+            
+            bundles.Add(jqueryBundle);
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
