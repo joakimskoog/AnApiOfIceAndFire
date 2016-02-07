689d340e5318ea9d9a5ff10a86ee83450893dc95
Joakim Skoog <glasvegas@hotmail.se>
2016-02-07 22:23:19 +0100

    Code for feeding database

---
+++ AnApiOfIceAndFire/App_Start/DataFeederActivator.cs
@@ -0,0 +1,16 @@
+﻿[assembly: WebActivatorEx.PostApplicationStartMethod(typeof(AnApiOfIceAndFire.DataFeederActivator), "Start")]
+
+namespace AnApiOfIceAndFire
+{
+    public class DataFeederActivator
+    {
+        public static void Start()
+        {
+            /*
+             * Uncomment the row below if you want to create a local database and fill it with data.
+             * Note that this can take up to 2 minutes the first time. After that it only takes a few seconds.
+             */
+            //Feeder.FeedDatabase(new AnApiOfIceAndFireContext(), "urlToBooksHere", "urlToCharactersHere", "urlToHousesHere", s => Debug.WriteLine(s));
+        }
+    }
+}
