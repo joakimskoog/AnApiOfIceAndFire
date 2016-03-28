4f61caef836f07d3afbe7509325c9db47397b086
Joakim Skoog <glasvegas@hotmail.se>
2016-03-28 17:26:38 +0200

    Removed dead code

--- AnApiOfIceAndFire.Data.Feeder/BookDto.cs
+++ AnApiOfIceAndFire.Data.Feeder/BookDto.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 namespace AnApiOfIceAndFire.Data.Feeder
 {
-    public class BookDto : IEquatable<BookDto>
+    public class BookDto
     {
         public int Id { get; set; }
         public string Name { get; set; }
@@ -17,46 +17,6 @@ namespace AnApiOfIceAndFire.Data.Feeder
         public string Country { get; set; }
         public DateTime ReleaseDate { get; set; }
         public int? PrecededBy { get; set; }
-        public int? FollowedBy { get; set; }
-
-        public bool Equals(BookDto other)
-        {
-            if (other == null) return false;
-
-            return Id == other.Id
-                   && string.Equals(Name, other.Name)
-                   && string.Equals(ISBN, other.ISBN)
-                   && Authors.SequenceEqual(other.Authors) //Note that this will return false if there are same authors, but in different order.
-                   && NumberOfPages == other.NumberOfPages
-                   && string.Equals(Publisher, other.Publisher)
-                   && string.Equals(MediaType, other.MediaType)
-                   && string.Equals(Country, other.Country)
-                   && ReleaseDate == other.ReleaseDate
-                   && PrecededBy == other.PrecededBy
-                   && FollowedBy == other.FollowedBy;
-        }
-
-        public override bool Equals(object obj)
-        {
-            return Equals(obj as BookDto);
-        }
-
-        public override int GetHashCode()
-        {
-            int hash = 17;
-
-            hash = hash * 23 + Id.GetHashCode();
-            hash = hash * 23 + Name.GetHashCode();
-            hash = hash * 23 + Authors.GetHashCode();
-            hash = hash * 23 + NumberOfPages.GetHashCode();
-            hash = hash * 23 + Publisher.GetHashCode();
-            hash = hash * 23 + MediaType.GetHashCode();
-            hash = hash * 23 + Country.GetHashCode();
-            hash = hash * 23 + ReleaseDate.GetHashCode();
-            hash = hash * 23 + PrecededBy.GetHashCode();
-            hash = hash * 23 + FollowedBy.GetHashCode();
-
-            return hash;
-        }
+        public int? FollowedBy { get; set; }   
     }
 }
