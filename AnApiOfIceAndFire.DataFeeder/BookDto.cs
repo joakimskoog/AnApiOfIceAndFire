34b4cdc75d775607d6e7d577eceb5c50082b29d0
joakimskoog <joakimskooog@gmail.com>
2017-07-08 15:28:10 +0200

    Removed comments

--- AnApiOfIceAndFire.DataFeeder/BookDto.cs
+++ AnApiOfIceAndFire.DataFeeder/BookDto.cs
@@ -17,22 +17,3 @@ namespace AnApiOfIceAndFire.DataFeeder
         public int? FollowedBy { get; set; }
     }
 }
-
-
-//public string ISBN { get; set; }
-
-//internal string AuthorsRaw { get; set; }
-//public string[] Authors
-//{
-//get => AuthorsRaw.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
-//internal set => AuthorsRaw = string.Join(";", value);
-//}
-
-//public int NumberOfPages { get; set; }
-//public string Publisher { get; set; }
-//public MediaType MediaType { get; set; }
-//public string Country { get; set; }
-//public DateTime ReleaseDate { get; set; }
-
-//public ICollection<int> CharacterIdentifiers { get; set; } = new List<int>();
-//public ICollection<int> PovCharacterIdentifiers { get; set; } = new List<int>();
