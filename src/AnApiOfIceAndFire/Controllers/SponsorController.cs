7fcfa955a4b8a37c255db5650f0f850bc5baed08
Joakim Skoog <joakimskooog@gmail.com>
2018-01-19 18:33:30 +0100

    Information about sponsorship

---
+++ src/AnApiOfIceAndFire/Controllers/SponsorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AnApiOfIceAndFire.Controllers
+{
+    public class SponsorController : Controller
+    {
+        [ResponseCache(Location = ResponseCacheLocation.Any, Duration = 600)]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
