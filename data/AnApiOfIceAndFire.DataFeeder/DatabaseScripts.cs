d3a5243cfabfa2f4472d63e4e872b4550978c37f
joakimskoog <joakimskooog@gmail.com>
2022-11-06 16:17:41 +0100

    Upgrade all projects to .NET 6

--- data/AnApiOfIceAndFire.DataFeeder/DatabaseScripts.cs
+++ data/AnApiOfIceAndFire.DataFeeder/DatabaseScripts.cs
@@ -3,10 +3,10 @@
     public class DatabaseScripts
     {
         public static readonly string CreateDatabase = @"CREATE DATABASE [anapioficeandfire] CONTAINMENT = NONE ON  PRIMARY 
-            ( NAME = N'anapioficeandfire', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL13.SQLEXPRESS\MSSQL\DATA\anapioficeandfire.mdf' , 
+            ( NAME = N'anapioficeandfire', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\anapioficeandfire.mdf' , 
                 SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
             LOG ON 
-            ( NAME = N'anapioficeandfire_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL13.SQLEXPRESS\MSSQL\DATA\anapioficeandfire_log.ldf' , 
+            ( NAME = N'anapioficeandfire_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\anapioficeandfire_log.ldf' , 
                 SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )";
 
         public static readonly string DeleteDatabase = @"DROP DATABASE [anapioficeandfire]";
